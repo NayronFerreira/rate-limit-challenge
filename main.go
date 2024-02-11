65bb268fdc7815a0fe80e2f20a5be6e6913b5912
NayronFerreira <nayronferreiradev@gmail.com>
2024-02-11 19:28:57 -0300

    [RateLimiter] - refactor func main.

--- main.go
+++ main.go
@@ -14,33 +14,48 @@ import (
 )
 
 func main() {
-	config, err := config.LoadConfig()
+	cfg := loadConfig()
+	rateLimiter := setupRateLimiter(cfg)
+	startServer(cfg, rateLimiter)
+}
+
+func loadConfig() *config.Config {
+	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatal("Error loading config:", err)
 	}
+	return cfg
+}
 
-	redisClient := database.NewRedisClient(config)
+func setupRateLimiter(cfg *config.Config) *ratelimiter.RateLimiter {
+	redisClient := database.NewRedisClient(cfg)
+	dbRedisDataLimiter := database.NewRedisDataLimiter(redisClient)
+	rateLimiter := ratelimiter.NewLimiter(dbRedisDataLimiter, cfg.TokenMaxRequestsPerSecond, int64(cfg.LockDurationSeconds), int64(cfg.BlockDurationSeconds), int64(cfg.IPMaxRequestsPerSecond))
 
-	dbRedis := database.NewRedisDataLimiter(redisClient)
-
-	rateLimiter := ratelimiter.NewLimiter(dbRedis, config.TokenMaxRequestsPerSecond, int64(config.LockDurationSeconds), int64(config.BlockDurationSeconds), int64(config.IPMaxRequestsPerSecond))
-
-	if err = rateLimiter.RegisterPersonalizedTokens(context.Background()); err != nil {
+	if err := rateLimiter.RegisterPersonalizedTokens(context.Background()); err != nil {
 		log.Fatal("Erro ao registrar o token:", err)
 	}
 
-	rateLimitMiddleware := middleware.RateLimitMiddleware(http.DefaultServeMux, rateLimiter)
+	return rateLimiter
+}
 
-	srv := server.New(config.WebPort, rateLimitMiddleware)
+func startServer(cfg *config.Config, rateLimiter *ratelimiter.RateLimiter) {
+	mux := http.NewServeMux()
+	setupRoutes(mux)
 
-	http.HandleFunc("/", handler.RootHandler)
+	rateLimitMiddleware := middleware.RateLimitMiddleware(mux, rateLimiter)
+	srv := server.New(cfg.WebPort, rateLimitMiddleware)
 
 	go func() {
-		log.Println("Servidor HTTP iniciado na porta:", config.WebPort)
+		log.Println("Servidor HTTP iniciado na porta:", cfg.WebPort)
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatal("Erro ao iniciar o servidor:", err)
 		}
 	}()
 
 	server.WaitForShutdown(srv)
+}
+
+func setupRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("/", handler.RootHandler)
 }
